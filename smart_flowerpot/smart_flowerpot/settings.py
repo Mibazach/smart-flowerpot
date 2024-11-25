93bb4ea51e02d3bfdbfdc00e5177c72210a91d43
Michal Lobaza <lobazamichal@student.agh.edu.pl>
2024-11-25 02:07:27 +0100

    chart progress

--- smart_flowerpot/smart_flowerpot/settings.py
+++ smart_flowerpot/smart_flowerpot/settings.py
@@ -101,7 +101,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = '/login/'
-#LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
 MESSAGE_TAGS = {
